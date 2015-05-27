6e6282b364b815abed98673b70f04e64860ff8f6
Adam Braus <ajbraus@gmail.com>
2015-05-27 14:36:45 -0700

    cleaning up questions ctrl

--- app/assets/javascripts/controllers/questionCtrls.js
+++ app/assets/javascripts/controllers/questionCtrls.js
@@ -6,7 +6,7 @@ angular.module('rangular')
     .controller('QuestionIndexCtrl', ['$scope', '$http', 'Question', function ($scope, $http, Question) {
         // $scope.questions = Question.all();
 
-        $scope.questions = $http.get('http://localhost:3000/api/questions.json')
+        $http.get('http://localhost:3000/api/questions.json')
           .success(function (data) {
             console.log(data);
             $scope.questions = data;
